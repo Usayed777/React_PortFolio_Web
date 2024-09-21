a95496a779476fe7289ad395b9dccd70892349e6
Usayed Afridi <108787705+Usayed777@users.noreply.github.com>
2024-09-21 13:12:34 +0500

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import aboutImg from "../assets/About.jpg";
+import aboutImg from "../assets/about.webp";
 import { ABOUT_TEXT } from '../constants';
 import { motion } from "framer-motion";
 
