84a7eed135ca82c9b911117e3c431cb63c483dbc
Usayed Afridi <108787705+Usayed777@users.noreply.github.com>
2024-09-21 17:13:00 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/Profile.png";
+import profilePic from "../assets/profile.webp";
 import { motion } from "framer-motion"
 
 const container = (delay) => ({
